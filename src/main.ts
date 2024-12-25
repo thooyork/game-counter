ef3fe28c0219f85d654204efae5efea46aa822e2
Thomas Haaf <thomas@smart-sign.com>
2024-12-25 22:25:45 +0100

    try better wakelock solution

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import './style.css'
 import { setupBuzzer } from './buzzer.ts'
-import NoSleep from '@uriopass/nosleep.js';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -8,8 +7,29 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div id="reset" class="reset">RESET</div>
   </div>
 `
-var noSleep = new NoSleep();
-noSleep.enable();
 
 setupBuzzer(document.querySelector<HTMLDivElement>('#buzzer')!, document.querySelector<HTMLDivElement>('#reset')!)
 
+let wakelock: any;
+
+function releaseWakeState() {
+  if(wakelock) wakelock.release();
+  wakelock = null;
+}
+
+const canWakeLock = () => 'wakeLock' in navigator;
+
+async function lockWakeState() {
+  if(!canWakeLock()) return;
+  try {
+    wakelock = await navigator.wakeLock.request();
+    wakelock.addEventListener('release', () => {
+      console.log('Screen Wake State Locked:', !wakelock.released);
+    });
+    console.log('Screen Wake State Locked:', !wakelock.released);
+  } catch(e) {
+    console.error('Failed to lock wake state with reason:', e.message);
+  }
+}
+
+await lockWakeState();
